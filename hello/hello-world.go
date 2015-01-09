10fd97258f7704d9bc55c14fd52f1e5124f908ae
Jared Harley <jared@jaredharley.com>
2015-01-08 19:05:09 -0700

    Created a stringutil library
    Created a stringutil library with a reverse function that reverses
    the characters of a string, and modified the hello world exmaple
    to use the new stringutil library.
    
    From: https://golang.org/doc/code.html

--- hello/hello-world.go
+++ hello/hello-world.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"github.com/jaredharley/learn-go/stringutil"
+)
 
 func main() {
 	fmt.Print("Hello, world!\n")
+	fmt.Printf(stringutil.Reverse("Hello, world!"))
+	fmt.Print("\n")
 }
